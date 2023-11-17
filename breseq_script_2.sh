f7459762e743d684f96cc89812a20644a395ddda
Oscar Delaney <o.delaney@uq.net.au>
2023-11-17 15:47:21 +1000

    -p flag based on clonal or not

--- breseq_script_2.sh
+++ breseq_script_2.sh
@@ -27,6 +27,20 @@ get_ref_genome() {
     fi
 }
 
+# Function to check for "clon" in the third column
+check_for_clon() {
+    SAMPLE_ID=$1
+    THIRD_COLUMN=$(awk -v ID="$SAMPLE_ID" 'BEGIN{FS="\t"} $1==ID {print $3}' "$SEQUENCES_DIR/samples.tsv")
+    
+    # Enable case-insensitive matching
+    shopt -s nocasematch
+    if [[ $THIRD_COLUMN == *"clon"* ]]; then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
+
 # Iterate over the R1 files in the sequences folder
 for R1_FILE in "$SEQUENCES_DIR"/**/*_R1_001.fastq.gz; do
     # Extract the sample ID from the R1 filename
@@ -34,6 +48,7 @@ for R1_FILE in "$SEQUENCES_DIR"/**/*_R1_001.fastq.gz; do
 
     # Get the appropriate reference genome for the sample ID
     REF_GENOME=$(get_ref_genome $SAMPLE_ID)
+    USE_P_FLAG=$(check_for_clon $SAMPLE_ID)
     
     # If no reference genome is found, continue to the next iteration
     if [ -z "$REF_GENOME" ]; then
@@ -44,8 +59,13 @@ for R1_FILE in "$SEQUENCES_DIR"/**/*_R1_001.fastq.gz; do
     # Construct the path to the corresponding R2 file
     R2_FILE="${R1_FILE/_R1_/_R2_}"
 
-    # Construct the breseq command
-    BRESEQ_CMD="breseq -r $REF_GENOME $R1_FILE $R2_FILE -p -j $NUM_THREADS -n $SAMPLE_ID -o $OUTPUT_DIR/$SAMPLE_ID"
+    # Define the main part of the breseq command
+    BRESEQ_CMD="breseq -r $REF_GENOME $R1_FILE $R2_FILE -j $NUM_THREADS -n $SAMPLE_ID -o $OUTPUT_DIR/$SAMPLE_ID"
+
+    # Conditionally append the -p flag
+    if [ "$USE_P_FLAG" == "true" ]; then
+        BRESEQ_CMD+=" -p"
+    fi
 
     # Run the breseq command and capture the exit status
     echo "Running breseq for $SAMPLE_ID..."
